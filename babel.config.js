7edc19019eb71a0f3936f13df8e4ea9295842213
Julian Rüth <julian.rueth@fsfe.org>
2023-02-13 17:09:57 +0100

    Use vite to build package

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-const appPresets = ['@vue/babel-preset-app', "@babel/preset-typescript"];
-const libPresets = ['@babel/preset-env', "@babel/preset-typescript"];
-module.exports = {
-  presets: (process.title === 'webpack' ? appPresets : libPresets),
-};
