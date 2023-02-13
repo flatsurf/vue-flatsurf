b2217025dab4cb1ea314270ff34dd106b2dcfc56
Julian Rüth <julian.rueth@fsfe.org>
2023-02-13 17:00:55 +0100

    Fix deployment rules
    untested but at least we are using vite now.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path, { resolve } from "path";
 import vue from '@vitejs/plugin-vue'
 
 const projectRoot = __dirname;
@@ -20,5 +20,19 @@ export default {
     environment: "jsdom",
     setupFiles: "./test/jsdom.mock.js",
   },
-  assetsInclude: ['**/*.txt'],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/entry.ts'),
+      name: 'vue-flatsurf',
+      fileName: 'vue-flatsurf',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 }
